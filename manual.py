42fc295a044750730286c0d5f06314eba4cf82d9
DTransaction <danny613tran@gmail.com>
2022-09-05 17:58:54 -0400

    Attempt to test setup

---
+++ manual.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO #Library for the GPIO Pins
+from time import sleep#Library for time-related tasks
+
+GPIO.setmode(GPIO.BCM) #Sets the way we reference the GPIO Pins
+
+pins = [26, 19, 13, 6, 22, 27, 17, 23]
+
+dat = pins[0]
+clk = pins[1]
+ltc = pins[2]
+clr = pins[3]
+d1 = pins[4]
+d2 = pins[5]
+d3 = pins[6]
+d4 = pins[7]
+
+for i in range(8): #Sets up GPIO pins as outputs
+    GPIO.setup(pins[i], GPIO.OUT) 
+
+GPIO.output((dat, d1, d2, d3), 1)
+GPIO.output((d1, d2, d3), 0)
+
+for blip in range(8): 
+    GPIO.output(clk, 1)
+    sleep(0.1)
+    GPIO.output(clk, 0)
+    sleep(0.1)
+
+
+GPIO.output(ltc, 1)
+sleep(0.1)
+GPIO.output(ltc, 0)
+GPIO.output(clr, 1)
+sleep(0.1)
+GPIO.output(clr, 0)
+
+
+GPIO.cleanup()
