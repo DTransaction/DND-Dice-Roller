c30607779457aaa88f8b643934de82a02a912eda
DTransaction <danny613tran@gmail.com>
2022-09-11 15:15:36 -0400

    trying 1234

--- manual.py
+++ manual.py
@@ -14,7 +14,6 @@ d2 = pins[5]
 d3 = pins[6]
 d4 = pins[7]
 
-
 letter_1  = [0, 0, 0, 0, 0, 1, 1, 0]
 letter_2  = [0, 1, 0, 1, 1, 0, 1, 1]
 letter_3  = [0, 1, 0, 0, 1, 1, 1, 1]
@@ -25,7 +24,7 @@ for i in range(4): #Sets up GPIO pins as outputs
     GPIO.setup(ltc, GPIO.OUT) 
     GPIO.setup(clk, GPIO.OUT) 
     GPIO.setup(clr, GPIO.OUT) 
-for i in range(4): #Sets up GPIO pins as outputs
+for i in range(4): #Sets up GPIO pins as outputs 
     GPIO.setup((d1, d2, d3, d4), GPIO.OUT) 
 
 GPIO.output((dat, clr), 1)
@@ -57,31 +56,31 @@ for x in range(1000):
     GPIO.output(ltc, 0)
     sleep(0.005)
 
-    GPIO.output((d1, d2, d4), 1)
-    GPIO.output((d3), 0)
-    for x in letter_3: 
-        GPIO.output(dat, x)
-        GPIO.output(clk, 0)
-        GPIO.output(clk, 1)
+    # GPIO.output((d1, d2, d4), 1)
+    # GPIO.output((d3), 0)
+    # for x in letter_3: 
+    #     GPIO.output(dat, x)
+    #     GPIO.output(clk, 0)
+    #     GPIO.output(clk, 1)
 
 
-    GPIO.output(ltc, 1)
-    GPIO.output(ltc, 0)
-    sleep(0.005)
+    # GPIO.output(ltc, 1)
+    # GPIO.output(ltc, 0)
+    # sleep(0.005)
 
-    GPIO.output((d1, d3, d2), 1)
-    GPIO.output((d4), 0)
-    for x in letter_4: 
-        GPIO.output(dat, x)
-        GPIO.output(clk, 0)
-        GPIO.output(clk, 1)
-
-    GPIO.output(ltc, 1)
-    GPIO.output(ltc, 0)
-    sleep(0.005)
+    # GPIO.output((d1, d3, d2), 1)
+    # GPIO.output((d4), 0)
+    # for x in letter_4: 
+    #     GPIO.output(dat, x)
+    #     GPIO.output(clk, 0)
+    #     GPIO.output(clk, 1)
+    # GPIO.output(ltc, 1)
+    # GPIO.output(ltc, 0)
+    # sleep(0.005)
 
 
 
 sleep(3)
 
+GPIO.output(clr, 0)
 GPIO.cleanup()
