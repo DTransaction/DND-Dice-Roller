31f19f9623a3334bf83031726381ec74524b0aea
DTransaction <danny613tran@gmail.com>
2022-09-22 00:22:20 -0400

    testing

--- manual.py
+++ manual.py
@@ -54,7 +54,6 @@ def release():
 def clean():
     GPIO.output(CLEAR, 0)
     GPIO.output(CLEAR, 1)
-    release()
 
 def display_character(character: str): 
     for status in CHARACTER[character]:
