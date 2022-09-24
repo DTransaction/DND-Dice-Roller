976a2d5fc73af41a70be0d49a043348a0dbd462f
DTransaction <danny613tran@gmail.com>
2022-09-24 19:48:04 -0400

    testing button, board 40

--- testing.py
+++ testing.py
@@ -1,9 +1,18 @@
-from time import sleep
-import random
+import time
+import RPi.GPIO as GPIO
 
-for x in range(10):
-    value = str(random.randint(1, 20))
-    if len(value) == 1:
-        value = "X" + value
-    print("XX" + value)
-    sleep(0.01*(x**2) + 0.1)
+#General Setup
+pins = [40]
+GPIO.setmode(GPIO.BOARD)  # Sets how we reference GPIO pins
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
+GPIO.setwarnings(False)
+print("Begin")
+
+try:
+    while True:
+        while GPIO.input(40) == GPIO.HIGH:
+            print("hi")
+
+except KeyboardInterrupt:
+    GPIO.cleanup() #cleansup all of the GPIO pins used within the script
+    print("Done") #informs the user the program is finished running
