80a4f52d6101788f4a7280378597f6e93424a988
DTransaction <danny613tran@gmail.com>
2022-09-30 21:46:12 -0400

    testing LED

--- testing.py
+++ testing.py
@@ -1,17 +1,21 @@
-import time
-import RPi.GPIO as GPIO
+import time 
+import RPi.GPIO as GPIO 
 
-#General Setup
-pins = [40]
-GPIO.setmode(GPIO.BOARD)  # Sets how we reference GPIO pins
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
+#General Setup 
+pin = 37
+GPIO.setmode(GPIO.BOARD)  # Sets how we reference GPIO pins 
+GPIO.setup(pin, GPIO.OUT)
 GPIO.setwarnings(False)
 print("Begin")
 
 try:
     while True:
-        while GPIO.input(40) == GPIO.HIGH:
-            print("hi")
+        user_input = ""
+        user_input = input("turn on LED? enter '1'")
+        if user_input == "1":
+            GPIO.output(pin, 1)
+            time.sleep(3)
+            GPIO.output(pin, 0)
 
 except KeyboardInterrupt:
     GPIO.cleanup() #cleansup all of the GPIO pins used within the script
